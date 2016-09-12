1174891a6d7d61e18564c2fc9e3cccb74fcc297f
Denis T <job@denis-it.com>
2016-09-12 16:47:10 +0300

    get_contacts request implemented

--- main.js
+++ main.js
@@ -5,12 +5,27 @@ const client = require('./lib/client')
 const app = express()
 
 app.get('/get_user_id_by_phone', (req, res) => {
-  res.send('Not implemented yet.')
+  res.send('not implemented yet')
 })
 
 app.get('/set_auth_code', (req, res) => {
   client.signIn(req.query.code)
     .then(() => res.send('ok'))
+})
+
+app.get('/get_contacts', (req, res) => {
+  client.getContacts()
+    .then(contacts => res.send(JSON.stringify(contacts)))
+})
+
+app.get('/import_contact', (req, res) => {
+  client.importContacts([{
+    client_id: 0,
+    phone: req.query.phone,
+    first_name: 'first_name',
+    last_name: 'last_name'
+  }], false)
+    .then(imported => res.send(JSON.stringify(imported)))
 })
 
 app.listen(3000, () => {
