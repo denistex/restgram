a177fcd35245616c87c6f94bea6fdaee670d9db8
Denis T <job@denis-it.com>
2016-09-30 15:29:21 +0300

    clean json responses for import_contact and get_full_user implemented

--- main.js
+++ main.js
@@ -4,43 +4,63 @@ const client = require('./lib/client')
 
 const app = express()
 
-app.get('/get_user_id_by_phone', (req, res) => {
-  res.send('not implemented yet')
-})
-
 app.get('/sign_in', (req, res) => {
   client.signIn(req.query.code)
-    .then(() => res.send('ok'))
-    .catch(() => res.send('error'))
+    .then(() => res.status(200).json({status: 'ok'}))
+    .catch(() => res.status(400).json({status: 'error', type: 'unknown'}))
 })
 
 app.get('/get_contacts', (req, res) => {
   client.getContacts()
-    .then(contacts => res.send(JSON.stringify(contacts)))
-    .catch(error => res.send(JSON.stringify(error)))
+    .then(contacts => res.status(200).json(contacts))
+    .catch(error => res.status(400).json(error))
 })
 
 app.get('/get_full_user', (req, res) => {
   client.getFullUser(req.query.user_id)
-    .then(user => res.send(JSON.stringify(user)))
-    .catch(error => res.send(JSON.stringify(error)))
+    .then(user => {
+      const result = { status: 'error', type: 'unknown' }
+      try {
+        res.status(200).json({
+          status: 'ok',
+          data: {
+            user_id: user.user.id,
+            first_name: user.user.first_name,
+            last_name: user.user.last_name
+          }
+        })
+      } catch (error) {
+        res.status(400).json(result)
+      }
+    })
+    .catch(error => res.status(400).json(error))
 })
 
 app.get('/check_phone', (req, res) => {
   client.checkPhone(req.query.phone)
-    .then(result => res.send(JSON.stringify(result)))
-    .catch(error => res.send(JSON.stringify(error)))
+    .then(result => res.status(200).json(result))
+    .catch(error => res.status(400).json(error))
 })
 
 app.get('/import_contact', (req, res) => {
   client.importContacts([{
     client_id: 0,
     phone: req.query.phone,
-    first_name: 'first_name',
-    last_name: 'last_name'
+    first_name: req.query.first_name || 'UNKNOWN',
+    last_name: req.query.last_name || 'UNKNOWN'
   }], false)
-    .then(imported => res.send(JSON.stringify(imported)))
-    .catch(error => res.send(JSON.stringify(error)))
+    .then(imported => {
+      const result = { status: 'error', type: 'unknown' }
+      try {
+        res.status(200).json({
+          status: 'ok',
+          data: { user_id: imported.users.list[0].id }
+        })
+      } catch (error) {
+        res.status(400).json(result)
+      }
+    })
+    .catch(error => res.status(400).json(error))
 })
 
 app.listen(3000, () => {
